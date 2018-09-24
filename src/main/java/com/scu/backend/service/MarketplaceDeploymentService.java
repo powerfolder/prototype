1c0f19cbab264111f7440129d7ca5b801a79c740
Mark Breuer <asinus666@googlemail.com>
2018-09-24 23:05:09 +0200

    Added deployment of market service.

--- src/main/java/com/scu/backend/service/MarketplaceDeploymentService.java
+++ src/main/java/com/scu/backend/service/MarketplaceDeploymentService.java
@@ -1,12 +1,22 @@
 package com.scu.backend.service;
 
+import org.web3j.protocol.core.RemoteCall;
+
+import com.scu.contract.generated.SCUMarketplace;
+
 public class MarketplaceDeploymentService {
 
 	private Web3jConnection web3jConnection;
 
 	public MarketplaceDeploymentService(Web3jConnection web3jConnection) {
-		super();
 		this.web3jConnection = web3jConnection;
+	}
+ 
+	public String deployMarketplace() throws Exception {
+		RemoteCall<SCUMarketplace> marketPlaceDeploymentCall = SCUMarketplace.deploy(web3jConnection.getWeb3j(),
+				web3jConnection.getCredentials(), web3jConnection.getGasPrice(), web3jConnection.getGasLimit());
+		SCUMarketplace myMarketplace = marketPlaceDeploymentCall.send();
+		return myMarketplace.getContractAddress();
 	}
 
 }
